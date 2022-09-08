562db3a4d32f34ca58130e7a09d4f3dbe34546ba
Alexander Latyshev <imposya0@gmail.com>
2022-09-08 15:09:29 +0300

    made employee

--- src/main/java/ru/imposya/task/dao/EmployeeDAO.java
+++ src/main/java/ru/imposya/task/dao/EmployeeDAO.java
@@ -13,4 +13,46 @@ import java.util.List;
 @Component
 public class EmployeeDAO {
 
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public EmployeeDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Employee> index() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("select p from Employee p", Employee.class)
+                .getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public Employee show(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Employee.class, id);
+    }
+
+    @Transactional
+    public void save(Employee employee) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(employee);
+    }
+
+    @Transactional
+    public void update(int id, Employee updatedEmployee) {
+        Session session = sessionFactory.getCurrentSession();
+        Employee employeeToBeUpdated = session.get(Employee.class, id);
+
+        employeeToBeUpdated.setName(updatedEmployee.getName());
+        employeeToBeUpdated.setAge(updatedEmployee.getAge());
+        employeeToBeUpdated.setEmail(updatedEmployee.getEmail());
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Employee.class, id));
+    }
 }
